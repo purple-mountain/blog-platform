08f0a55a97f6c6be61e0c5f5b8abdf15a7d5c2df
purple-mountain <lmwrong92@gmail.com>
2024-10-08 20:32:37 +0000

    refactor: replaced raw sql queries with typeorm repository methods

--- src/modules/users/users.repository.ts
+++ src/modules/users/users.repository.ts
@@ -1,65 +1,22 @@
-import { db } from "#/database/database";
-import { randomUUID } from "crypto";
+import { AppDataSource } from "#/database/database";
 import { SignUpRequestBodyDto } from "../auth/dto/request/sign-up-request-body.dto";
-import { UserModel } from "./users.model";
+import { Repository } from "typeorm";
+import { User } from "./entities/user.entity";
 
 export class UsersRepository {
-	private static readonly userColumns: string = `id, email, username, password, role, created_at as createdAt`;
+	private static repository: Repository<User> = AppDataSource.getRepository(User);
 
-	static async getOneByEmail({ email }: { email: string }): Promise<UserModel | null> {
-		const [user] = await db.execute<UserModel>(
-			`
-        SELECT
-            ${this.userColumns}
-        FROM
-            users
-        WHERE
-            email = $1;
-    `,
-			[email]
-		);
-
-		if (!user) {
-			return null;
-		}
-
-		return user;
+	static async getOneByEmail({ email }: { email: string }): Promise<User | null> {
+		return await this.repository.findOne({ where: { email } });
 	}
 
-	static async getOneById({ id }: { id: string }): Promise<UserModel | null> {
-		const [user] = await db.execute<UserModel>(
-			`
-        SELECT
-            ${this.userColumns}
-        FROM
-            users
-        WHERE
-            id = $1;
-    `,
-			[id]
-		);
-
-		if (!user) {
-			return null;
-		}
-
-		return user;
+	static async getOneById({ id }: { id: string }): Promise<User | null> {
+		return await this.repository.findOne({ where: { id } });
 	}
 
-	static async createOne(user: SignUpRequestBodyDto): Promise<UserModel> {
-		const [newUser] = await db.execute<UserModel>(
-			`
-        INSERT INTO users (id, email, username, password)
-        VALUES ($1, $2, $3, $4)
-        RETURNING ${this.userColumns};
-    `,
-			[randomUUID(), user.email, user.username, user.password]
-		);
+	static async createOne(user: SignUpRequestBodyDto): Promise<User> {
+		const newUser = this.repository.create(user);
 
-		if (!newUser) {
-			throw new Error("Database error");
-		}
-
-		return newUser;
+		return await this.repository.save(newUser);
 	}
 }
