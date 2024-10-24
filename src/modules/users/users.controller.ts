f2ff6d9c16f1ddee6d7e0f743a89b2ae320d6b09
purple-mountain <lmwrong92@gmail.com>
2024-10-24 16:00:39 +0000

    feat: added endpoints for getting, updating user profile and updating password

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -9,8 +9,53 @@ import { uuidSchema } from "#/shared/schemas/uuid.schema";
 import { auth } from "#/shared/middlewares/auth.middleware";
 import { requireRole } from "#/shared/middlewares/require-role.middleware";
 import { UserRole } from "#/shared/constants/user-role.constant";
+import { updateUserProfileRequestBodyDtoSchema } from "./dto/request/update-user-profile-request-body.dto";
+import { updateUserPasswordRequestBodyDtoSchema } from "./dto/request/update-user-password-request-body.dto";
 
 export const UsersController = Router();
+
+UsersController.get(
+	"/profile",
+	auth,
+
+	async (req: Request, res: Response) => {
+		const user = await UsersService.getProfile(req.user.id);
+
+		return res.status(200).json({
+			data: user,
+			message: "User profile retrieved successfully",
+		});
+	}
+);
+
+UsersController.put(
+	"/profile",
+	auth,
+	validateRequestBody(updateUserProfileRequestBodyDtoSchema),
+
+	async (req: Request, res: Response) => {
+		const updatedUser = await UsersService.updateProfile(req.body, req.user.id);
+
+		return res.status(200).json({
+			data: updatedUser,
+			message: "User profile updated successfully",
+		});
+	}
+);
+
+UsersController.put(
+	"/profile/password",
+	auth,
+	validateRequestBody(updateUserPasswordRequestBodyDtoSchema),
+
+	async (req: Request, res: Response) => {
+		await UsersService.updatePassword(req.body, req.user.id);
+
+		return res.status(200).json({
+			message: "User password updated successfully",
+		});
+	}
+);
 
 UsersController.put(
 	"/:userId/role",
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,9 +1,65 @@
+import bcrypt from "bcrypt";
 import { User } from "./entities/user.entity";
 import { UpdateUserRoleRequestBodyDto } from "./dto/request/update-user-role-request-body.dto";
 import { UsersRepository } from "./users.repository";
 import { NotFoundError } from "#/shared/errors/not-found.error";
+import { UpdateUserProfileRequestBodyDto } from "./dto/request/update-user-profile-request-body.dto";
+import { UpdateUserPasswordRequestBodyDto } from "./dto/request/update-user-password-request-body.dto";
+import { UnauthorizedError } from "#/shared/errors/unauthorized.error";
 
 export class UsersService {
+	static async getProfile(userId: string): Promise<User> {
+		const user = await UsersRepository.getOneById({ id: userId });
+
+		if (!user) {
+			throw new NotFoundError("User not found");
+		}
+
+		return user;
+	}
+
+	static async updateProfile(
+		dto: UpdateUserProfileRequestBodyDto,
+		userId: string
+	): Promise<User> {
+		const updatedUser = await UsersRepository.updateOne(dto, userId);
+
+		if (!updatedUser) {
+			throw new NotFoundError("User not found");
+		}
+
+		return updatedUser;
+	}
+
+	static async updatePassword(
+		dto: UpdateUserPasswordRequestBodyDto,
+		userId: string
+	): Promise<void> {
+		const user = await UsersRepository.getOneById({ id: userId });
+
+		if (!user) {
+			throw new NotFoundError("User not found");
+		}
+
+		const isPasswordCorrect = await bcrypt.compare(dto.currentPassword, user.password);
+
+		if (!isPasswordCorrect) {
+			throw new UnauthorizedError("Password is incorrect");
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashedNewPassword = await bcrypt.hash(dto.newPassword, salt);
+
+		const updatedUser = await UsersRepository.updateOne(
+			{ password: hashedNewPassword },
+			userId
+		);
+
+		if (!updatedUser) {
+			throw new NotFoundError("User not found");
+		}
+	}
+
 	static async updateUserRole(
 		dto: UpdateUserRoleRequestBodyDto,
 		userId: string
